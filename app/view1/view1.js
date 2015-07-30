9d6a73ad7b1abeb8fa725ec3fff581dbb1962709
Miika <mfabianad@gmail.com>
2015-07-29 23:16:45 -0300

    firs commit

---
+++ app/view1/view1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', view1Config])
+.controller('View1Ctrl', [function() {
+
+}])
+function view1Config($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}
+
+view1Config['$inject'] = ['$routeProvider'];
